241652a9b31f09de98c5e626beda15657c253ff3
Cristian Simandan <andrei.simandan96@e-uvt.ro>
2017-08-16 13:48:39 +0300

    Cristi

---
+++ mavenproject3/src/main/java/com/msgsystems/mavenproject3/App.java
@@ -0,0 +1,16 @@
+package com.msgsystems.mavenproject3;
+import java.util.logging.*;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static Logger log = Logger.getLogger("training log");
+    public static void main( String[] args )
+    {
+        log.log(Level.FINE,"gigi");
+        System.out.println( "Hello World!" );
+        
+    }
+}
